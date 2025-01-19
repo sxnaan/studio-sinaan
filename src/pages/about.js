b93e8d91b8b8e5726c7fd72352986adef89378ea
sinaan <sinaan.younus@gmail.com>
2025-01-19 14:30:57 -0600

    cleanup

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,11 +13,10 @@ const About = () => {
             <div className="page-container">                
                 <h1 className='page-title'>About</h1>
                 <p>SiNAAN ABDURREHMAN YOUNUS is a 23 year old mixed media artist from Baltimore.</p>
-                <p>He first studied fine art at Perry Hall Middle School under Sarah Deacon, and later at the University of Maryland with Emily Conover, Brandon Donahue, and Cy Keener, amongst others. 
+                <p>He first studied fine art at Perry Hall Middle School with Sarah Deacon, and later at the University of Maryland under Emily Conover, Brandon Donahue, and Cy Keener, amongst others. 
                     He has worked across a full range of wet and dry media. He has also explored digital fabrication (3D printing/laser cutting) and augmented/virtual reality.</p>
-                <p>His work is driven by a single question — <em>Can we create the art that we enjoy consuming?</em></p>
-                <p>To him, it is not just possible, but necessary, for that answer to be yes.</p>  
-                <br></br>
+                <p>His work is driven by a single question – can we create the art that we enjoy consuming? <em>To him, it is not just possible, but necessary, for that answer to be yes.</em></p>  
+                <p>—</p>
                 
                 <b>Currently based in Chicago, working primarily with oil.</b>
                 
